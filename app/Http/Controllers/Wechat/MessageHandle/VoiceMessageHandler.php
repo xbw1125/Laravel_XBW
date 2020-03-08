15bcad9ec5be2af3eb777c8c5baaf35a30b8d76a
xibinwei <1790704571@qq.com>
2020-03-08 15:22:31 +0800

    添加语音消息处理

--- app/Http/Controllers/Wechat/MessageHandle/VoiceMessageHandler.php
+++ app/Http/Controllers/Wechat/MessageHandle/VoiceMessageHandler.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Wechat\MessageHandle;
 
 use EasyWeChat\Kernel\Contracts\EventHandlerInterface;
+use Illuminate\Support\Facades\Log;
 
 class VoiceMessageHandler implements EventHandlerInterface
 {
     public function handle($payload = null)
     {
+        Log::info($payload);
         return $payload['Recognition'];
     }
 }
