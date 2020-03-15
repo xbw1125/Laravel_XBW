7f72700f8de06b0e09089b838950cb2aa89c403c
xibinwei <1790704571@qq.com>
2020-03-15 19:20:21 +0800

    更改+8时区；获取用户基本信息

---
+++ app/Http/Controllers/Wechat/User/UserInfoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\WeChat\User;
+
+use EasyWeChat\Factory;
+
+class UserInfoController
+{
+    public function getUserInfo()
+    {
+        $app = Factory::officialAccount(config('wechat.official_account.default'));
+        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
+        dd($user);
+        $userInfo = $app->user->get($user['openid']);
+        dd($userInfo);
+    }
+}
--- routes/wechat.php
+++ routes/wechat.php
@@ -2,9 +2,7 @@
 
 Route::any('/server', 'WeChatServerController@index');
 
-Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
-    Route::get('/user', function () {
-        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
-        dd($user);
-    });
+Route::group(['middleware' => ['web', 'wechat_oauth']], function () {
+
+    Route::any('/user', 'User\UserInfoController@getUserInfo');
 });
